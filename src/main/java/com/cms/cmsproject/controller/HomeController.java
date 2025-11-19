5a1b80e3c94b0c7a47c92c8ef86929ad3e88eb34
Pavan Kumar Bestha <bpavankumarbestha@gmail.com>
2025-11-19 18:58:10 +0530

    Update HomeController.java

--- src/main/java/com/cms/cmsproject/controller/HomeController.java
+++ src/main/java/com/cms/cmsproject/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.cmsproject.controller;
+package com.cms.cmsproject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
